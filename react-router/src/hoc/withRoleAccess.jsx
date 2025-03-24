55b93ac50bac97467bc6d1dc9325cbe7ae15516e
13Puru <purab.das@teamkarimganj.com>
2025-03-24 11:28:07 +0100

    Update

--- react-router/src/hoc/withRoleAccess.jsx
+++ react-router/src/hoc/withRoleAccess.jsx
@@ -5,10 +5,10 @@ const withRoleAccess = (WrappedComponent) => {
   return (props) => {
     // Try to get userRole from props, then from localStorage if not found
     const userRole = props.userRole || localStorage.getItem('userRole');
-    console.log("withRoleAccess using userRole:", userRole);
+    // console.log("withRoleAccess using userRole:", userRole);
     
     const isAuthorized = userRole === "admin" || userRole === "agent";
-    console.log("isAuthorized calculated as:", isAuthorized);
+    // console.log("isAuthorized calculated as:", isAuthorized);
     
     return <WrappedComponent {...props} isAuthorized={isAuthorized} />;
   };
